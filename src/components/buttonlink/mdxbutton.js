150f12bc5436c96637546239eb041adae464ab45
Beau Jackson <beauhaus19@gmail.com>
2019-12-14 04:04:37 -0500

    renamed btnlink

---
+++ src/components/buttonlink/mdxbutton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import btnlinkStyles from './mdxbutton.module.scss';
+
+const Btn = ({ btnText }) => (
+    <div className={btnlinkStyles.container}>
+        <button className={btnlinkStyles.button}>{btnText}</button>
+        {/* <img src={} alt="this is my grandma"/> */}
+    </div>
+)
+
+export default Btn;
