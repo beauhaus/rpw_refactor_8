eb78807a0576bb70642aa903899d4582e93fd7ea
Beau Jackson <beauhaus19@gmail.com>
2019-12-17 20:19:12 -0500

    centers text in nav

--- src/components/calendar/calendarnav.js
+++ src/components/calendar/calendarnav.js
@@ -3,14 +3,21 @@ import moment from 'moment'
 
 import calNavStyles from './calendarnav.module.scss';
 
-const CalNav = ({decMonth, incMonth, selectedDate}) => {
+const CalNav = ({ decMonth, incMonth, selectedDate }) => {
     // console.log("> calNav", selectedDate)
     return (
         <nav className={calNavStyles.nav}>
-            <button onClick={decMonth}>&#x3c;</button>
-            <h1>{moment(selectedDate).format("MMMM")}</h1>
-            <button onClick={incMonth}>&#x3e;</button>
+            <div className={calNavStyles.subContainer}>
+                <button className={`${calNavStyles.prevBtn} ${calNavStyles.calNavBtn}`} onClick={decMonth}>
+                    <span className={calNavStyles.prev}></span>
+                </button>
+                <h1>{moment(selectedDate).format("MMMM")}</h1>
+                <button className={`${calNavStyles.nextMonth} ${calNavStyles.calNavBtn}`} onClick={incMonth}>
+                    <span className={calNavStyles.next}></span>
+                </button>
+            </div>
         </nav>
-    )}
+    )
+}
 
 export default CalNav;
