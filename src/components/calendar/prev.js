e2e6e92f37347c66c27e36e0c04d0b2b849cb648
Beau Jackson <beauhaus19@gmail.com>
2019-12-21 15:42:06 -0500

    adds webkit touch

--- src/components/calendar/prev.js
+++ src/components/calendar/prev.js
@@ -4,21 +4,20 @@ import Locky from "react-locky";
 import prevStyles from './prev.module.scss';
 
 
-const Prev = () =>{
-    const text = Array(10)
-  .fill(1)
-  .map((x, index) => <div key={index}>{index}</div>);
+const Prev = () => {
+    const text = Array(30)
+        .fill(1)
+        .map((x, index) => <div key={index}>{index}</div>);
 
-return  (
-    <div className={prevStyles.container}>
-      <Locky className={prevStyles.locky}>
-          
-      <div className={prevStyles.content}>
-      {text}
-  </div>
-     
-      </Locky>
-    </div>
-  )};
+    return (
+        <div className={prevStyles.container}>
+            <Locky className={prevStyles.locky}>
+                <div className={prevStyles.content}>
+                    {text}
+                </div>
+            </Locky>
+        </div>
+    )
+};
 
 export default Prev;
