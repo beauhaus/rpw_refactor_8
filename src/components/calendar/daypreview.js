b3fcb07d38e0472fa7eca1f65b9d9cd0fef1e8a4
Beau Jackson <beauhaus19@gmail.com>
2019-12-20 23:34:47 -0500

    omg

--- src/components/calendar/daypreview.js
+++ src/components/calendar/daypreview.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { gsap } from "gsap";
+// import { gsap } from "gsap";
 import moment from 'moment';
 import { Link } from 'gatsby'
 import Locky from "react-locky";
@@ -14,34 +14,48 @@ import dayPreviewStyles from './daypreview.module.scss';
 const DayPreview = ({ previewDate }) => {
     const [DBCallParameter, setDBCallParameter] = useState('default')
 
-    useEffect(() => {
-        setDBCallParameter(previewDate)
-    }, [previewDate])
+    // useEffect(() => {
+    //     setDBCallParameter(previewDate)
+    // }, [previewDate])
 
-    let dateRef = useRef(null)
-    let listRef = useRef(null)
+    // let dateRef = useRef(null)
+    // let listRef = useRef(null)
 
-    useEffect(() => {
-        gsap.set(dateRef, { opacity: 0, y: -100 })
-        gsap.set(listRef, { opacity: 0, y: 30 })
+    // useEffect(() => {
+    //     gsap.set(dateRef, { opacity: 0, y: -100 })
+    //     gsap.set(listRef, { opacity: 0, y: 30 })
 
-        const tl = gsap.timeline({ defaults: { opacity: 1 } })
+    //     const tl = gsap.timeline({ defaults: { opacity: 1 } })
 
-        tl.to(dateRef, { duration: .6, y: 0, ease: 'elastic(1,0.8)', delay: 0.2 })
-            .to(listRef, { duration: .8, y: 0, ease: "back(1.4)" }, '-=0.5')
-    }, [previewDate])
+    //     tl.to(dateRef, { duration: .6, y: 0, ease: 'elastic(1,0.8)', delay: 0.2 })
+    //         .to(listRef, { duration: .8, y: 0, ease: "back(1.4)" }, '-=0.5')
+    // }, [previewDate])
 
     const db = allEventsData(DBCallParameter)
     // let overflow = {
     //     "  -webkit-overflow-scrolling": "touch"
     // }
     /***********************/
-    return (
-        <Locky enabled={false}>
-            <div className={dayPreviewStyles.container}>
+    const text = ["Oh, My God!", "Oh, My God!", "Oh, My God!", "Oh, My God!", "Oh, My God!", "Oh, My God!", "Oh, My God!", "Oh, My God!"];
 
-                <h1 ref={elem => dateRef = elem}>{moment(previewDate).format("MMM DD")}</h1>
-                <ul ref={elem => listRef = elem}
+    return (
+        <div className={dayPreviewStyles.container}>
+
+            {/* <h1 ref={elem => dateRef = elem}>{moment(previewDate).format("MMM DD")}</h1> */}
+            <Locky enabled={false}>
+                <div
+                    style={{
+                        backgroundColor: "skyblue",
+                        overflow: "scroll",
+                        height: 100,
+                        width: 200
+                    }}
+                >
+                    {text.map((item, idx) => (
+                        <h1>{item}</h1>
+                    ))}
+                </div>
+                {/* <ul ref={elem => listRef = elem}
                     className={dayPreviewStyles.previewList} >
                     {db.map((item, idx) => (
                         <li key={idx} className={dayPreviewStyles[`${item.event.stylecode}`]}>
@@ -52,10 +66,10 @@ const DayPreview = ({ previewDate }) => {
                             <hr />
                         </li>
                     ))}
-                </ul>
+                </ul> */}
+            </Locky>
 
-            </div>
-        </Locky>
+        </div>
     )
 }
 
