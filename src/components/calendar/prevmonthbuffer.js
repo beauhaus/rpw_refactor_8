1e1d2cc656bcdf3217f1d8aa179e38b361e5f1aa
Beau Jackson <beauhaus19@gmail.com>
2019-12-24 19:17:35 +0100

    touchup ... now correct first day

--- src/components/calendar/prevmonthbuffer.js
+++ src/components/calendar/prevmonthbuffer.js
@@ -3,10 +3,10 @@ import moment from 'moment'
 
 const PrevMonthBuffer = ({ selectedDate }) => {
     /* uncomment this */
-    // const firstDay = moment(selectedDate).startOf("month").format('d');
+    const firstDay = moment(selectedDate).startOf("month").format('d');
 
    
-    const firstDay = "5";  // DELETE THIS
+    // const firstDay = "5";  // DELETE THIS
     return (
         <span style={1 * firstDay ? { gridColumn: `${firstDay}` } : { display: 'none' }} />
     )
