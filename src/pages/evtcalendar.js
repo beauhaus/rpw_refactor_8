541c9611ca1808c2893872b7bdfb1de783a38989
Beau Jackson <beauhaus19@gmail.com>
2019-12-28 16:46:56 +0100

    scroll works in own page?

--- src/pages/evtcalendar.js
+++ src/pages/evtcalendar.js
@@ -1,16 +1,20 @@
 import React from 'react'
-import Calendar from '../components/calendar/calendar'
+import Locky from "react-locky";
+
+// import Calendar from '../components/calendar/calendar'
 import evtCalStyles from './evtcalendar.module.scss'
-import Header from '../components/header'
+// import Header from '../components/header'
+
+import LockyWrap from '../components/calendar/lockywrappanel'
+
 
 const EvtCalendar = () => {
     // console.log(Insta)
     return (
             <main className={evtCalStyles.container}>
-                <Header/>
-
-                <Calendar/>
-                
+                {/* <Header/> */}
+                {/* <Calendar/> */}
+                <LockyWrap/>
             </main>
     )
 }
