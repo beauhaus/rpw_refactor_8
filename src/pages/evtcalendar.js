bc385866a37c77f31daa0610e028bc298fde8870
Beau Jackson <beauhaus19@gmail.com>
2019-12-28 19:32:06 +0100

    adds daygrid

--- src/pages/evtcalendar.js
+++ src/pages/evtcalendar.js
@@ -1,22 +1,53 @@
-import React from 'react'
-// import Locky from "react-locky";
-
-// import Calendar from '../components/calendar/calendar'
-import evtCalStyles from './evtcalendar.module.scss'
-// import Header from '../components/header'
+import React, {useState} from 'react'
+import moment from 'moment'
+import { monthArrayMaker } from '../utils/utils'
 
 import LockyWrap from '../components/calendar/lockywrappanel'
 import CalNav from '../components/calendar/calnav'
+import DaysGrid from '../components/calendar/daysgrid'
+
+
+import evtCalStyles from './evtcalendar.module.scss'
+// import Header from '../components/header'
+
+
 
 const EvtCalendar = () => {
-    // console.log(Insta)
+    const [selectedDate, setSelectedDate] = useState(moment().format('YYYY-MM-DD'))
+
+    const [previewDate, setPreviewDate] = useState(moment().format('YYYY-MM-DD'))
+
+    const incrementMonth = () => {
+        let addMonth = moment(selectedDate).add(1, 'month').format("YYYY-MM-DD");
+        return setSelectedDate(addMonth)
+    }
+
+    const decrementMonth = () => {
+        let subtractMonth = moment(selectedDate).subtract(1, 'month').format("YYYY-MM-DD");
+        return setSelectedDate(subtractMonth)
+    }
+    const selectDay = (dateString) => {
+        console.log("selectDay Input:", dateString)
+        return setPreviewDate(dateString)
+    }
+
+    /********************************************************/
+
+    const daysArr = monthArrayMaker(selectedDate)
+    
     return (
-            <main className={evtCalStyles.container}>
-               
-                <button onClick={() => alert("You clicked!")}>click me</button>
-                <CalNav/>
-                <LockyWrap className={evtCalStyles.div2}/>
-            </main>
+        <main className={evtCalStyles.container}>
+            <CalNav 
+            previewDate={previewDate}
+            />
+            <DaysGrid
+            daysArr={daysArr}
+            selectedDate={selectedDate}
+            dayClick={selectDay} 
+            />
+            <button onClick={() => alert("You clicked!")}>click me</button>
+            <LockyWrap className={evtCalStyles.div2} />
+        </main>
     )
 }
 
