3b8eb3b04f8a200ba9bb8068df6a71bb300e4d12
Beau Jackson <beauhaus19@gmail.com>
2019-12-29 16:33:04 +0100

    styled evts with stylecodes & classes added

--- src/components/calendar/calnav.js
+++ src/components/calendar/calnav.js
@@ -6,13 +6,13 @@ import calNavStyles from './calnav.module.scss';
 const CalNav = ({ decMonth, incMonth, selectedDate }) => {
     return (
         <nav className={calNavStyles.nav}>
-                <button className={calNavStyles.calNavBtn} onClick={decMonth}>
-                    <span>&#x2039;</span>
-                </button>
-                <h1>{moment(selectedDate).format("MMMM")}</h1>
-                <button className={calNavStyles.calNavBtn} onClick={incMonth}>
-                    <span>&#x203A;</span>
-                </button>
+            <button className={calNavStyles.calNavBtn} onClick={decMonth}>
+                <span>&#x2039;</span>
+            </button>
+            <h1>{moment(selectedDate).format("MMMM")}</h1>
+            <button className={calNavStyles.calNavBtn} onClick={incMonth}>
+                <span>&#x203A;</span>
+            </button>
         </nav>
     )
 }
--- src/components/calendar/panelevents.js
+++ src/components/calendar/panelevents.js
@@ -6,14 +6,16 @@ import { Link } from 'gatsby';
 import eventStyles from './panelevents.module.scss';
 
 const PanelEvents = ({ db }) => {
+    console.table(db)
     return (
         db.map((item, idx) => (
             <li key={idx} className={eventStyles[`${item.event.stylecode}`]}>
                 <h3 className={eventStyles.evtTitle}>{item.event.title}</h3>
                 <span className={eventStyles.evtHours}>{item.event.hours}</span>
                 <p className={eventStyles.evtDesc}>{item.event.desc}</p>
+                {/* <hr className={eventStyles.evtDivider} /> */}
+                <br/>
                 <Link className={eventStyles.evtLink} to={item.event.slug}>{item.event.slug ? "find out more!" : "link to event page"}</Link>
-                <hr className={eventStyles.evtDivider} />
             </li>
         ))
     )
