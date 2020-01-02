50475018db93004a2cdac18aa0e8da19460dbd81
Beau Jackson <beauhaus19@gmail.com>
2020-01-03 00:30:12 +0100

    styling added to nav

--- src/components/calendar/calnav.js
+++ src/components/calendar/calnav.js
@@ -1,13 +1,11 @@
 import React from "react"
 import moment from 'moment'
-// import rpwOrbBlue from '../../../images/rpw-calendar-orb.png'
 
 import calNavStyles from './calnav.module.scss';
 
 const CalNav = ({ decMonth, incMonth, selectedDate }) => {
     return (
         <nav className={calNavStyles.nav}>
-            {/* <img className={calNavStyles.rpwIcon} src={rpwOrbBlue} alt="rpw-icon-blue" /> */}
 
             <button className={calNavStyles.calNavBtn} onClick={decMonth}>
                 <span>&#x2039;</span>
